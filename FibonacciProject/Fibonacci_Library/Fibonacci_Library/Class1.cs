a71fa87892cf51d8982360944b3d2ab5eade231b
rododendron1001 <60620826+rododendron1001@users.noreply.github.com>
2020-03-22 00:34:02 +0200

    sir Fibonacci

---
+++ FibonacciProject/Fibonacci_Library/Fibonacci_Library/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Fibonacci_Library
+{
+    public class Fibonacci_Class
+    {
+        public static int[] Fibonacci(int number, string arrayName)
+        {
+            int[] array1 = new int[number];
+            array1[0] = 0;
+            array1[1] = 1;
+            for (int i = 2; i < number; i++)
+            {
+                array1[i] = array1[i - 2] + array1[i - 1];
+            }
+            return array1;
+        }
+    }
+}
