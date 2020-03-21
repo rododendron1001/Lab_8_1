54bdd65288c2f7cbf22ab90bf0bb77687dd1d2f2
rododendron1001 <60620826+rododendron1001@users.noreply.github.com>
2020-03-21 22:55:05 +0200

    sir Fibonacci

---
+++ FibonacciProject/FibonacciProject/For_Console.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FibonacciProject
+{
+    class For_Console
+    {
+        public static int ReadNumber(string message, int maxAttempts, int defaultValue)
+        {
+            int attemptsCount = 1;
+            bool isNumber = false;
+
+            while (!isNumber)
+            {
+                Console.Write(message);
+                string text = Console.ReadLine();
+
+                isNumber = int.TryParse(text, out int number) && (number > 0);
+
+                if (isNumber)
+                {
+                    return number;
+                }
+                else
+                {
+                    Console.WriteLine($"'{text}' is not a number, try again ({maxAttempts - attemptsCount} attempts remaining) ...");
+                }
+
+                attemptsCount++;
+
+                if (attemptsCount > maxAttempts)
+                {
+                    Console.WriteLine($"Max attempts count exceeded, asuming {defaultValue} as default value ...");
+                    return defaultValue;
+                }
+            }
+
+            return defaultValue;
+        }
+
+        public static void PrintArray(string arrayName, int[] array)
+        {
+            string arrayElements = string.Join(", ", array ?? new int[] { });
+
+            Console.WriteLine($"{arrayName} = [{arrayElements}]");
+        }
+    }
+}
---
+++ FibonacciProject/FibonacciProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FibonacciProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VectorFibonacci();
+        }
+
+        private static void VectorFibonacci()
+        {
+            int n = For_Console.ReadNumber("Elementul \"N\" (final) sir Fibonacci : ", 3, 0);
+            int[] Fibonacci_array = Fibonacci_Library.Fibonacci_Class.Fibonacci(n, "Fibonacci_Array");
+            For_Console.PrintArray("Fibonacci_array", Fibonacci_array);
+        }
+    }
+}
